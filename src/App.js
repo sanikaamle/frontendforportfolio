ba387aa0bdff59dbfd0903023a356930332c8ea6
sanikaamle <sanika.amle123@gmail.com>
2025-07-17 23:50:50 +0530

    usereffect removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 import ProfileCard from './ProfileCard';
 import MagnetLines from './MagnetLines';
